c94f7a9da8ae21e8522cc5c672823cbb87b784a4
riyenas0925 <riyenas0925@gmail.com>
2022-04-09 13:49:14 +0900

    feat: SkillPayload - action 구현 [#14]
    * 실행되는 스킬의 정보를 담고있는 필드입니다. 사용자의 발화로부터 추출한 엔티티의 값을 추가적으로 포함합니다. (공식 문서)
    * Action, DetailParams 구현
    * skill_payload 테스트 json에 detailParams와 params 내용 추가
    
    Closed #14

--- src/main/java/dev/mini/skill/payload/SkillPayload.java
+++ src/main/java/dev/mini/skill/payload/SkillPayload.java
@@ -3,13 +3,17 @@ package dev.mini.skill.payload;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.mini.skill.payload.action.Action;
 import dev.mini.skill.payload.bot.Bot;
 import dev.mini.skill.payload.intent.Intent;
 import dev.mini.skill.payload.userrequest.UserRequest;
 
+import java.util.Objects;
+
 public class SkillPayload {
     private Bot bot;
     private Intent intent;
+    private Action action;
     private UserRequest userRequest;
 
     public Bot getBot() {
@@ -20,8 +24,25 @@ public class SkillPayload {
         return intent;
     }
 
+    public Action getAction() {
+        return action;
+    }
+
     public UserRequest getUserRequest() {
         return userRequest;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SkillPayload that = (SkillPayload) o;
+        return Objects.equals(bot, that.bot) && Objects.equals(intent, that.intent) && Objects.equals(action, that.action) && Objects.equals(userRequest, that.userRequest);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bot, intent, action, userRequest);
     }
 
     @Override
@@ -29,6 +50,7 @@ public class SkillPayload {
         return "SkillPayload{" +
                 "bot=" + bot +
                 ", intent=" + intent +
+                ", action=" + action +
                 ", userRequest=" + userRequest +
                 '}';
     }
---
+++ src/main/java/dev/mini/skill/payload/action/DetailParams.java
@@ -0,0 +1,44 @@
+package dev.mini.skill.payload.action;
+
+import java.util.Objects;
+
+public class DetailParams {
+    private String origin;
+    private String value;
+    private String groupName;
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DetailParams that = (DetailParams) o;
+        return Objects.equals(origin, that.origin) && Objects.equals(value, that.value) && Objects.equals(groupName, that.groupName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(origin, value, groupName);
+    }
+
+    @Override
+    public String toString() {
+        return "DetailParams{" +
+                "origin='" + origin + '\'' +
+                ", value='" + value + '\'' +
+                ", groupName='" + groupName + '\'' +
+                '}';
+    }
+
+}
